33e00fee24710ff38d2879097b1012ec184e2634
T. Nishimura <tnish@fastmail.jp>
2022-01-03 17:31:38 -0800

    init.sql sample data split off into own file

--- UrlShortener/init.sql
+++ UrlShortener/init.sql
@@ -7,14 +7,3 @@ create table shortened_urls (
     short_code text not null
 );
 
-insert into shortened_urls (
-    submitter_ip, full_url, short_code
-) 
-values 
-( '127.0.0.1' , 'https://www.google.com'    , '1000001')  ,
-( '127.0.0.1' , 'https://www.panasonic.com' , '1000001')  ,
-( '127.0.0.1' , 'https://www.yahoo.com'     , '1000002')  ,
-( '127.0.0.1' , 'https://myanimelist.net'   , '1000003')  ,
-( '127.0.0.1' , 'https://www.github.com'    , '1000004')  ,
-( '127.0.0.1' , 'https://www.microsoft.com' , '1000005');
-
